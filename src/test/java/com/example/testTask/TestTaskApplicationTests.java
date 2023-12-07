13bd9f7d86d752083e8079a9d3289a820d4abc47
VladimirJG <vladimi85@gmail.com>
2023-12-07 14:37:29 +0300

    refactoring

--- src/test/java/com/example/testTask/TestTaskApplicationTests.java
+++ src/test/java/com/example/testTask/TestTaskApplicationTests.java
@@ -1,12 +1,7 @@
 package com.example.testTask;
 
-import com.example.testTask.Util.InputStringTestData;
-import com.example.testTask.dao.InputStringsDAO;
-import com.example.testTask.models.InputString;
-import jakarta.validation.Valid;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.example.testTask.Util.InputStringTestData.ACTUAL_STRING;
@@ -16,11 +11,11 @@ import static com.example.testTask.Util.InputStringTestData.EMPTY_STRING;
 class TestTaskApplicationTests {
 
 
-
     @Test
     void actualStringInput() {
-		Assertions.assertTrue(ACTUAL_STRING.length()>5);
+        Assertions.assertTrue(ACTUAL_STRING.length() > 5);
     }
+
     @Test
     void emptyStringInput() {
         Assertions.assertEquals(0, EMPTY_STRING.length());
