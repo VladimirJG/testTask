39c62e188b17f5475fa724ca12a465a9e0d19046
VladimirJG <vladimi85@gmail.com>
2023-12-07 14:20:52 +0300

    added test.

--- src/test/java/com/example/testTask/TestTaskApplicationTests.java
+++ src/test/java/com/example/testTask/TestTaskApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.testTask;
 
+import com.example.testTask.Util.InputStringTestData;
+import com.example.testTask.dao.InputStringsDAO;
+import com.example.testTask.models.InputString;
+import jakarta.validation.Valid;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.example.testTask.Util.InputStringTestData.ACTUAL_STRING;
+import static com.example.testTask.Util.InputStringTestData.EMPTY_STRING;
 
 @SpringBootTest
 class TestTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+
+    @Test
+    void actualStringInput() {
+		Assertions.assertTrue(ACTUAL_STRING.length()>5);
+    }
+    @Test
+    void emptyStringInput() {
+        Assertions.assertEquals(0, EMPTY_STRING.length());
+    }
 }
---
+++ src/test/java/com/example/testTask/Util/InputStringTestData.java
@@ -0,0 +1,11 @@
+package com.example.testTask.Util;
+
+public class InputStringTestData {
+    public static final String ACTUAL_STRING = "aafffgggkkkddd";
+    public static final String EMPTY_STRING = "";
+    public static final String LESS_STRING="avf";
+    public static final String BIG_CAPITAL_LETTER_STRING="Ghdgsss";
+    public static final String NOT_LATIN_STRING="абракодабра";
+    public static final String SPECIAL_CHARACTER_STRING="/*&^%$#";
+    public static final String SEVEN_SPACES_STRING="       ";
+}
