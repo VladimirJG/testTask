f23c0fe93c9f033720c802c1b7a74af3322a04c3
VladimirJG <vladimi85@gmail.com>
2023-12-08 11:32:35 +0300

    changed @Pattern

--- src/main/java/com/example/testTask/dto/InputStringsDTO.java
+++ src/main/java/com/example/testTask/dto/InputStringsDTO.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 public class InputStringsDTO {
     @NotEmpty(message = "Строка не может быть пустой")
     @Size(min = 5, message = "Строка не должна быть меньше 5 символов")
-    @Pattern(regexp = "[a-z]\\w+", message = "Ввод должен содержать строчные латинские буквы без пробелов. Пример: aaaaabcc")
+    @Pattern(regexp = "[a-z]+", message = "Ввод должен содержать строчные латинские буквы без пробелов. Пример: aaaaabcc")
     private String inputString;
 
 
